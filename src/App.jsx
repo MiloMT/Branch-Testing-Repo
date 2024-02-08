22e585fea0a3cc8e7c4f5610fd73290032e53331
Mfern10 <mitchellfernandez147@gmail.com>
2024-02-08 20:42:19 +0900

    Deleting stupid navbar component that didn't work haha

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/Card'
-import Navbar from './components/Navbar'
+
 
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-      <Navbar />
+      
       <Card />
       <div>
         <a href="https://vitejs.dev" target="_blank">
